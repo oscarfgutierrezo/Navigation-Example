fd04b964615fd1df3aaf114b16304897c05e788e
Oscar Gutierrez <oscarfgutierrezo@gmail.com>
2022-03-07 22:22:22 -0500

    Add: navigation-01 and navigation-02 sections

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        "background": "#F2F2F2",
+        "primary": "#2F1893",
+        "dark-violet": "#1E0E62",
+        "light-violet": "#482BE7"
+      },
+      fontFamily: {
+        "sans": "DM Sans"
+      }
+    },
+  },
+  plugins: [],
+}
