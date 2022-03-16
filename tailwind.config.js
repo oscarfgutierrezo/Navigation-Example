ccbf3dbfcdf8ea137928588ba5c48e0a0ee35d7b
Oscar Gutierrez <oscarfgutierrezo@gmail.com>
2022-03-16 09:25:53 -0500

    Add: navigation-23 section

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,15 @@ module.exports = {
       },
       backgroundImage: {
         nav22: 'url("../img/navigation-22.png")',
-    },
+        nav23a: 'url("../img/navigation-23-01.png")',
+        nav23b: 'url("../img/navigation-23-02.png")',
+        nav23c: 'url("../img/navigation-23-03.png")',
+        nav23d: 'url("../img/navigation-23-04.png")',
+        nav23e: 'url("../img/navigation-23-05.png")',
+      },
+      gridTemplateColumns: {
+        'fill': 'repeat(auto-fill, minmax(300px, 1fr))',
+      }
     },
   },
   plugins: [],
